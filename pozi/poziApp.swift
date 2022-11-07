0d2f5e33cb60f52c0d00d41a1fbae57564dd0a76
Gersai <gersai_ventura@hotmail.com>
2022-11-06 21:47:08 -0600

    Initial Commit

---
+++ pozi/poziApp.swift
@@ -0,0 +1,17 @@
+//
+//  poziApp.swift
+//  pozi
+//
+//  Created by eduardo gersai  on 06/11/22.
+//
+
+import SwiftUI
+
+@main
+struct poziApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
